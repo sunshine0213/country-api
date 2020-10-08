c8a4cf5c527bc1437ef83d0d5c61dfeda7653110
sunshine0213 <kyleflynn.johnson1125@gmail.com>
2020-10-08 23:05:01 +0300

    Router, home, tasks page and some style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,65 @@
 <template>
-  <div id="app">Let's do it!</div>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/"
+				>Home</router-link
+			>
+			|
+			<router-link to="/tasks"
+				>Tasks</router-link
+			>
+		</div>
+		<router-view />
+	</div>
 </template>
 
-<script>
-export default {
-  name: "App",
-  components: {},
-};
-</script>
-
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap');
+:root {
+	font-size: 8px;
+}
+body {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Nunito', sans-serif;
+	font-size: 2rem;
+}
+a {
+	text-decoration: none;
+}
+h1 {
+	font-style: normal;
+	font-weight: 500;
+	font-size: 25px;
+	line-height: 34px;
+	text-align: center;
+	letter-spacing: 0.02em;
+	color: #1a1b1d;
+	margin: 10px 0;
+}
+h2 {
+	font-style: normal;
+	font-weight: 500;
+	font-size: 20px;
+	line-height: 27px;
+	letter-spacing: 0.02em;
+	color: #1a1b1d;
+}
+p {
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 22px;
+	text-align: center;
+	letter-spacing: 0.02em;
+	color: #1a1b1d;
+	margin: 10px 0;
+}
+.primary-highlight {
+	color: #79a8ed;
+}
+.sub-text {
+	color: #a4b0cb;
+}
 </style>
---
+++ src/views/Tasks.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="tasks">
+		<div class="tasks__inner">
+			<h1>
+				Hello
+				<span class="primary-highlight"
+					>Bikash!</span
+				>
+			</h1>
+			<p class="sub-text">
+				26th October 2020
+			</p>
+			<img
+				src="@/assets/empty_illustration.svg"
+				class="home__hero-image"
+			/>
+			<p>
+				Your task list for today looks
+				empty
+			</p>
+			<Button label="Add a task" />
+		</div>
+	</div>
+</template>
+<script>
+import Button from '@/components/Button.vue'
+export default {
+	name: 'Tasks',
+	components: {
+		Button,
+	},
+}
+document.title = 'Tasks'
+</script>
+<style scoped>
+.tasks {
+	margin: 40px 30px;
+}
+.tasks__inner {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+.tasks p {
+	margin-top: 0px;
+}
+img {
+	margin: 20px 0;
+}
+</style>
