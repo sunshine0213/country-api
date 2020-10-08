58d0026ab41ae78f7aa58e2de90b0c87e146ae52
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-10-08 23:05:01 +0300

    Router, home, tasks page and some style

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+	<button class="button">
+		{{ label }}
+	</button>
+</template>
+<script>
+export default {
+	name: 'Button',
+	props: {
+		label: String,
+	},
+}
+</script>
+<style scoped>
+.button {
+	background-color: #79a8ed;
+	width: 100%;
+	padding: 10px;
+	border: none;
+	border-radius: 6px;
+	color: #ffffff;
+	font-size: 2rem;
+	font-weight: 400;
+	margin: 10px 0;
+}
+.button:hover {
+	background-color: #4887e4;
+	cursor: pointer;
+	border: none;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="home">
+		<div class="home__inner">
+			<img
+				src="@/assets/time_illustration.svg"
+				class="home__hero-image"
+			/>
+			<h1>
+				Tomorrow never comes
+				<span class="primary-highlight"
+					>Let’s do</span
+				>
+				it today!
+			</h1>
+			<Button label="Lets do it" />
+		</div>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Button from '@/components/Button.vue'
+
+export default {
+	name: 'Home',
+	components: { Button },
+	methods: {
+		onStart: function() {
+			console.log('hello')
+		},
+	},
+}
+document.title = 'Home'
+</script>
+<style scoped>
+.home {
+	margin: 40px 30px;
+}
+.home__inner {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+.home .home__hero-image {
+	margin-bottom: 20px;
+	display: flex;
+	justify-content: center;
+}
+</style>
