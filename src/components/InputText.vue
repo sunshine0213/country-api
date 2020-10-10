05358939de2a94da64fa4b23b3e8a88437325979
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-10-11 00:38:12 +0300

    Task complete checked , get started input

---
+++ src/components/InputText.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<label class="input-label">
+			{{ label }}
+		</label>
+		<input
+			type="text"
+			class="input-text"
+			placeholder="placeholder"
+		/>
+	</div>
+</template>
+<script>
+export default {
+	name: 'InputText',
+	props: {
+		label: String,
+		placeholder: String,
+	},
+}
+</script>
+<style scoped>
+.input-text {
+	margin-top: 15px;
+	border: none;
+	font-family: 'Nunito', sans-serif;
+	font-size: 2rem;
+	color: #a4b0cb;
+	border: 1px solid #a4b0cb;
+	width: 100%;
+	padding: 10px;
+	border-radius: 6px;
+}
+</style>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -14,7 +14,7 @@
 				src="@/assets/empty_illustration.svg"
 				class="home__hero-image"
 			/>
-			<p>
+			<p v-on:click="complete(2)">
 				Your task list for today looks
 				empty
 			</p>
@@ -22,26 +22,20 @@
 			<div class="task-list">
 				<h2>Today</h2>
 			</div>
+
 			<TodoItem
-				task="Call dad"
-				time="12:30"
-				isCompleted
-				priority="medium"
-			/>
-			<TodoItem
-				task="Send CV to peter"
-				time="13:30"
-				priority="low"
-			/>
-			<TodoItem
-				task="Pick pulled oats"
-				time="18:30"
-				priority="high"
+				v-for="item in items"
+				v-bind:key="item.id"
+				v-bind="item"
+				v-bind:complete="complete"
+				v-bind:edit="edit"
 			/>
 		</div>
 	</div>
 </template>
 <script>
+import { uuid } from 'vue-uuid'
+
 import Button from '@/components/Button.vue'
 import TodoItem from '@/components/TodoItem.vue'
 export default {
@@ -49,6 +43,60 @@ export default {
 	components: {
 		Button,
 		TodoItem,
+	},
+	data: function() {
+		return {
+			items: [
+				{
+					id: uuid.v4(),
+					task: 'Call dad',
+					time: '12:30',
+					isCompleted: false,
+					isEditing: false,
+					priority: 'high',
+				},
+				{
+					id: uuid.v4(),
+					task: 'Send CV to peter',
+					time: '14:30',
+					isCompleted: false,
+					isEditing: false,
+					priority: 'medium',
+				},
+				{
+					id: uuid.v4(),
+					task: 'Make a video',
+					time: '18:30',
+					isCompleted: false,
+					isEditing: false,
+					priority: 'low',
+				},
+			],
+		}
+	},
+	methods: {
+		complete: function(id) {
+			const _items = this.items.map(
+				(item) => {
+					if (item.id === id) {
+						item.isCompleted = !item.isCompleted
+					}
+					return item
+				},
+			)
+			this.items = [..._items]
+		},
+		edit: function(id) {
+			const _items = this.items.map(
+				(item) => {
+					if (item.id === id) {
+						item.isEditing = !item.isEditing
+					}
+					return item
+				},
+			)
+			this.items = [..._items]
+		},
 	},
 }
 document.title = 'Tasks'
