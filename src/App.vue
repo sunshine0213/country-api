13a58be12f9ff012ae240586ef4ea5f9049456c2
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-10-12 23:26:52 +0300

    Current time, vuex package, routing redirect from landing to task

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/dashboard/tasks">Tasks</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<router-view />
+	</div>
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap');
 
 :root {
-  font-size: 8px;
+	font-size: 8px;
 }
 body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Nunito", sans-serif;
-  font-size: 2rem;
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Nunito', sans-serif;
+	font-size: 2rem;
 }
 a {
-  text-decoration: none;
+	text-decoration: none;
 }
 h1 {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 25px;
-  line-height: 34px;
-  text-align: center;
-  letter-spacing: 0.02em;
-  color: #1a1b1d;
-  margin: 10px 0;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 25px;
+	line-height: 34px;
+	text-align: center;
+	letter-spacing: 0.02em;
+	color: #1a1b1d;
+	margin: 10px 0;
 }
 h2 {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 27px;
-  letter-spacing: 0.02em;
-  color: #1a1b1d;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 20px;
+	line-height: 27px;
+	letter-spacing: 0.02em;
+	color: #1a1b1d;
 }
 p {
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 22px;
-  text-align: center;
-  letter-spacing: 0.02em;
-  color: #1a1b1d;
-  margin: 10px 0;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 16px;
+	line-height: 22px;
+	text-align: center;
+	letter-spacing: 0.02em;
+	color: #1a1b1d;
+	margin: 10px 0;
 }
 .primary-highlight {
-  color: #79a8ed;
+	color: #79a8ed;
 }
 .sub-text {
-  color: #a4b0cb;
+	color: #a4b0cb;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,127 @@
 <template>
-  <div class="home">
-    <div class="home__inner">
-      <h1>
-        Hello
-        <span class="primary-highlight">{{ firstName }}!</span>
-      </h1>
+	<div class="home">
+		<div class="home__inner">
+			<h1>
+				Hello
+				<span class="primary-highlight"
+					>{{ firstName }}!</span
+				>
+			</h1>
 
-      <p class="sub-text">26th October 2020</p>
-      <img
-        v-if="items.length < 1 && !isAddTask"
-        src="@/assets/empty_illustration.svg"
-        class="home__hero-image"
-      />
-      <p v-if="items.length < 1 && !isAddTask">
-        Your task list for today looks empty
-      </p>
-      <Button
-        v-if="items.length < 1 && !isAddTask"
-        v-bind:onClick="toAddView"
-        label="Add a task"
-      />
+			<p class="sub-text">
+				{{ currentDate }}
+			</p>
+			<img
+				v-if="
+					items.length < 1 && !isAddTask
+				"
+				src="@/assets/empty_illustration.svg"
+				class="home__hero-image"
+			/>
+			<p
+				v-if="
+					items.length < 1 && !isAddTask
+				"
+			>
+				Your task list for today looks
+				empty
+			</p>
+			<Button
+				v-if="
+					items.length < 1 && !isAddTask
+				"
+				v-bind:onClick="toAddView"
+				label="Add a task"
+			/>
 
-      <router-view />
-    </div>
-  </div>
+			<router-view />
+		</div>
+	</div>
 </template>
 <script>
-import Button from "@/components/Button.vue";
+import Button from '@/components/Button.vue'
 export default {
-  name: "Home",
-  components: {
-    Button,
-  },
-  mounted: function () {
-    if (this.$router.currentRoute.name) {
-      this.isAddTask = true;
-    }
-  },
-  data: function () {
-    return {
-      firstName: JSON.parse(localStorage.getItem("taskapp")).firstName,
-      items: JSON.parse(localStorage.getItem("taskapp")).items,
-      isAddTask: false,
-    };
-  },
-  methods: {
-    toAddView: function () {
-      this.$router.push("/dashboard/add");
-    },
-  },
-};
-document.title = "Home";
+	name: 'Home',
+	components: {
+		Button,
+	},
+	mounted: function() {
+		if (
+			this.$router.currentRoute.name ==
+			'add'
+		) {
+			this.isAddTask = true
+		}
+		if (this.items.length > 0) {
+			this.isAddTask = false
+		}
+		this.currentDate = this.getCurrentTime()
+	},
+	data: function() {
+		return {
+			firstName: JSON.parse(
+				localStorage.getItem('taskapp'),
+			).firstName,
+			items: JSON.parse(
+				localStorage.getItem('taskapp'),
+			).items,
+			isAddTask: false,
+			currentDate: '',
+		}
+	},
+	methods: {
+		toAddView: function() {
+			this.isAddTask = true
+			this.$router.push(
+				'/dashboard/add',
+			)
+		},
+		getCurrentTime: function() {
+			const monthNames = [
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+				'July',
+				'August',
+				'September',
+				'October',
+				'November',
+				'December',
+			]
+			let dateObj = new Date()
+			let month =
+				monthNames[dateObj.getMonth()]
+			let day = String(
+				dateObj.getDate(),
+			).padStart(2, '0')
+			let year = dateObj.getFullYear()
+			let output =
+				day + ' ' + month + ' ' + year
+			return output
+		},
+	},
+}
+document.title = 'Home'
 </script>
 <style scoped>
 .home {
-  margin: 40px 30px;
+	margin: 40px 30px;
 }
 .home__inner {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 }
 .home p {
-  margin-top: 0px;
+	margin-top: 0px;
 }
 img {
-  margin: 20px 0;
+	margin: 20px 0;
 }
 .task-list {
-  margin-top: 20px;
+	margin-top: 20px;
 }
 </style>
