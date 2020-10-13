1f219e7a79334613dba1ccab07efd1abe3f22ffb
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-10-13 10:37:21 +0300

    Input placeholder, addTask page margin, route guard to nested routes

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,59 +1,70 @@
 <template>
-  <div class="input-text">
-    <label v-if="label" class="input-label">
-      {{ label }}
-    </label>
-    <input
-      type="text"
-      class="input-box"
-      placeholder="placeholder"
-      v-bind:value="model"
-      v-on:input="$emit('input', $event.target.value)"
-      v-focus
-    />
-  </div>
+	<div class="input-text">
+		<label
+			v-if="label"
+			class="input-label"
+		>
+			{{ label }}
+		</label>
+		<input
+			type="text"
+			class="input-box"
+			v-bind:placeholder="placeholder"
+			v-bind:value="model"
+			v-on:input="
+				$emit(
+					'input',
+					$event.target.value,
+				)
+			"
+			v-focus
+		/>
+	</div>
 </template>
 <script>
 export default {
-  name: "InputText",
-  props: {
-    label: String,
-    placeholder: String,
-    model: String,
-  },
-  data: function () {
-    return {
-      inputValue: "",
-    };
-  },
-  methods: {
-    inputChange: function () {
-      console.log("input value", this.inputValue);
-    },
-  },
-  directives: {
-    focus: {
-      // directive definition
-      inserted: function (el) {
-        el.focus();
-      },
-    },
-  },
-};
+	name: 'InputText',
+	props: {
+		label: String,
+		placeholder: String,
+		model: String,
+	},
+	data: function() {
+		return {
+			inputValue: '',
+		}
+	},
+	methods: {
+		inputChange: function() {
+			console.log(
+				'input value',
+				this.inputValue,
+			)
+		},
+	},
+	directives: {
+		focus: {
+			// directive definition
+			inserted: function(el) {
+				el.focus()
+			},
+		},
+	},
+}
 </script>
 <style scoped>
 .input-text {
-  margin-top: 20px;
+	margin-top: 20px;
 }
 .input-box {
-  margin-top: 15px;
-  border: none;
-  font-family: "Nunito", sans-serif;
-  font-size: 2rem;
-  color: #a4b0cb;
-  border: 1px solid #a4b0cb;
-  width: 100%;
-  padding: 10px;
-  border-radius: 6px;
+	margin-top: 15px;
+	border: none;
+	font-family: 'Nunito', sans-serif;
+	font-size: 2rem;
+	color: #a4b0cb;
+	border: 1px solid #a4b0cb;
+	width: 100%;
+	padding: 10px;
+	border-radius: 6px;
 }
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -24,7 +24,7 @@
 				<InputText
 					label="What should I call you?"
 					v-model="firstName"
-					placeholder="full name"
+					placeholder="Full name"
 				/>
 				<Button
 					v-if="isStart"
