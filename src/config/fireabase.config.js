c4762e9cd5e48640fbf8ed66efbcc603266e0244
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-10-25 22:27:31 +0200

    Firebase signup and login

--- src/config/fireabase.config.js
+++ src/config/fireabase.config.js
@@ -1,19 +1,9 @@
-import Vue from 'vue'
-
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-
-
-
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCOc9xqW5KRMJ6mu11ISkECNOujbxd9saw",
   authDomain: "vue-lets-do.firebaseapp.com",
   databaseURL: "https://vue-lets-do.firebaseio.com",
   projectId: "vue-lets-do",
   storageBucket: "vue-lets-do.appspot.com",
   messagingSenderId: "1002789362776",
-  appId: "1:1002789362776:web:98538f580b13eb9d677e2d"
+  appId: "1:1002789362776:web:4830bf71d1595fca677e2d"
 };
-firebase.initializeApp(firebaseConfig)
-export const db = firebase.firestore()
